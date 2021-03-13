e50caa1def1a6a9f3840be9470ab7124eb7c1779
silas <silas_jfex@nocafe.net>
2021-03-13 01:16:20 +0000

    Improve cache mechanism.  It works now, but still require some careful review and changes, specially regarding security.  It handles cache dir and XDG_CACHE_HOME correctly and we can now use it as a generic cache mechanism to store stuff other than access_token, but there are still some TODOs in this file that deserve more attention.
    FossilOrigin-Name: 0cfbd6e728969bdc8206b78282068aff1e1e3a9271a3bb6b77342f4dde774250

--- src/cache.c
+++ src/cache.c
@@ -1,28 +1,37 @@
+#include <assert.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 #include "cache.h"
 #include "utils.h"
 
 /*
- * TODO: for now, this is just a quick-&-dirty implementation of what will be a
- * cache mechanism for janechat.
- *
- * Special care with non-existing paths should be taken.
+ * We provide a very simple mechanism for persistence.  It is simple a key-value
+ * database, where each key-value pair is a file, where key is the filename and
+ * value its contents.
+ 
+ * Although it is possible, these files are not meant to be edited from an
+ * editor.  They do not have a end of line character at the end of the file and
+ * editing them can break janechat.
  */
+
+static char *cache_dir();
+static void mkdir_r(const char *);
 
 const char *
 cache_set(const char *key, const char *value)
 {
-	(void)key;
-	char *home = getenv("HOME");
+	mkdir_r(cache_dir());
+	
 	char path[PATH_MAX];
-	snprintf(path, sizeof(path), "%s/.config/janechat.cache", home);
+	snprintf(path, sizeof(path), "%s/%s", cache_dir(), key);
 
 	FILE *f = fopen(path, "w");
-	if (!f)
-		return NULL;
+	assert(f != NULL); /* TODO: handle this case */
 	fprintf(f, "%s", value);
+	fflush(f);
 	fclose(f);
 	return value;
 }
@@ -30,10 +39,8 @@ cache_set(const char *key, const char *value)
 char *
 cache_get(const char *key)
 {
-	(void)key;
-	char *home = getenv("HOME");
 	char path[PATH_MAX];
-	snprintf(path, sizeof(path), "%s/.config/janechat.cache", home);
+	snprintf(path, sizeof(path), "%s/%s", cache_dir(), key);
 	FILE *f = fopen(path, "r");
 	if (!f)
 		return NULL;
@@ -41,3 +48,54 @@ cache_get(const char *key)
 	fclose(f);
 	return contents;
 }
+
+/*
+ * Return a internal pointer to the cache directory.  This pointer is a static
+ * object and should not be freed.
+ */
+static char *
+cache_dir()
+{
+	static char dir[PATH_MAX] = { '\0' };
+
+	if (dir[0] != '\0')
+		return dir;
+
+	if (getenv("XDG_CACHE_HOME")) {
+		snprintf(dir, sizeof(dir), "%s/janechat",
+			getenv("XDG_CACHE_HOME"));
+		return dir;
+	}
+
+	assert(getenv("HOME") != NULL);
+	snprintf(dir, sizeof(dir), "%s/.cache/janechat", getenv("HOME"));
+	return dir;
+}
+
+/*
+ * Creates a directory tree.  It does that from the bottom-up, by checking if
+ * leaf exist before going to the parent.  It creates the parents recursivelly
+ * to create the leafs.
+ */
+static void
+mkdir_r(const char *path)
+{
+	struct stat s;
+	if (stat(path, &s) == 0) {
+		if (s.st_mode & S_IFDIR)
+			return;
+		else
+			assert(0 == 1); /* TODO: what if it is a non-directory? */
+	}
+	
+	char parent[PATH_MAX];
+	snprintf(parent, sizeof(parent), "%s", path);
+	char *c = strrchr(parent, '/');
+	*c = '\0';
+	mkdir_r(parent);
+	if (mkdir(path, 0700) != 0) {
+		perror("mkdir_r()"); /* TODO: handle errors correctly */
+		exit(1);
+	}
+}
+
