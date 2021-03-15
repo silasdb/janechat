4d8720fab1b303d4f2f45759b5a02df8c1533c50
silas <silas_jfex@silas.net.br>
2021-03-15 15:20:14 +0000

    Prefer all lowercase characters for file names.  Our initial idea was having some independent data structures (StrBuf, Hash, etc.) in their own files with the same name for the data structures, but it is not always possible to do, as we'll see when we make the room implementation, that is probably end up having other data structures other than the Room or Rooms struct.  So, using upper case characters makes things a bit more confusing and help little.  The same is not true for source code, where we still use CamelCase for our types, so they do not clash with C types and make them easier to spot.
    FossilOrigin-Name: a11a23592211632c19725934c4922769bcae1dad1e523339e3dcd5858a248f4a

--- src/hash.c
+++ src/hash.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "Hash.h"
+#include "hash.h"
 
 #define HASH_SIZE 256
 struct node {
--- src/strbuf.c
+++ src/strbuf.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "StrBuf.h"
+#include "strbuf.h"
 
 #define INITSIZE 256
 
