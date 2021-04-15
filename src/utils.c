5875c2b761324ed96907ad8358710d00b92dd212
silas <silas_jfex@nocafe.net>
2021-04-15 21:24:36 +0000

    Use libcurl instead of calling curl with popenve().  It is not only better because we avoid IPC via pipe, but we also have a more portable code, since popenve() is only implemented inNetBSD.  In the future we may have our own HTTP code, who knows?
    FossilOrigin-Name: 0e9a2b0b5ed6cb92b1a1b3af1952736d43899838e1cd3b34b2aad3735ccb7253

--- src/utils.c
+++ src/utils.c
@@ -8,9 +8,27 @@ char *read_line_alloc() {
 	char *line = NULL;
 	size_t linesize = 0, len;
 	len = getline(&line, &linesize, stdin);
-	assert(line != NULL);
-	assert(line[len-1] == '\n');
-	line[len-1] = '\0';
+	
+	/*
+	 * TODO: before using libcurl, we used to have:
+	 *
+	 * assert(line != NULL);
+	 *
+	 * because getline() blocked indepently of the process received SIGALRM.
+	 *
+	 * With libcurl, it just returns a NULL line whenever SIGALRM is sent
+	 * (see main.c). We decide to allow return of a NULL line to the caller
+	 * (this also needed an adaptation on the caller).
+	 *
+	 * It is not very clear why it happens, maybe curl configures signals so
+	 * they behave differently.  It also seems it uses SIGALRM in some cases.
+	 * I should study it more and write an appropriate technote about it.
+	 * For now, let's live with this change.
+	 */
+	if (line) {
+		assert(line[len-1] == '\n');
+		line[len-1] = '\0';
+	}
 	return line;
 }
 
