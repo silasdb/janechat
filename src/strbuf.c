41ca8d3860e2f2f4c1e6c64650353e0cc8c99719
silas <silas_jfex@silas.net.br>
2021-04-15 21:18:11 +0000

    Let the user detach a buffer from its StrBuf object.
    FossilOrigin-Name: 5efdb443d3cd9cbfe3e8e43beea0329f3ff8eac9bd355e29ff246a0e68c27e59

--- src/strbuf.c
+++ src/strbuf.c
@@ -11,6 +11,7 @@ struct StrBuf {
 	char *buf;
 	size_t len; // do not include null byte
 	size_t max;
+	bool attached;
 };
 
 char *strbuf_buf(const StrBuf *s) {
@@ -24,6 +25,7 @@ StrBuf *strbuf_new() {
 	ss->buf[0] = '\0';
 	ss->len = 0;
 	ss->max = INITSIZE;
+	ss->attached = true;
 	return ss;
 }
 
@@ -51,7 +53,8 @@ void strbuf_ncat_c(StrBuf *ss, const char *s, size_t size) {
 }
 
 void strbuf_free(StrBuf *ss) {
-	free(ss->buf);
+	if (ss->attached)
+		free(ss->buf);
 	free(ss);
 }
 
@@ -62,4 +65,15 @@ size_t strbuf_len(const StrBuf *ss) {
 void strbuf_reset(StrBuf *ss) {
 	ss->len = 0;
 	ss->buf[0] = '\0';
+}
+
+/*
+ * Detach a buffer from the StrBuf object.  This is useful when we need to work
+ * on the `char *` buffer directly and don't need the StrBuf object anymore.  The
+ * caller will have to call strbuf_free() on the StrBuf object and free() on the
+ * char * variable when after using the `char *` string.
+ */
+char *strbuf_detach(StrBuf *ss) {
+	ss->attached = false;
+	return ss->buf;
 }
