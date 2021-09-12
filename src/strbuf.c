3a64294de22a9402d69fd27d835065c54d8630a7
silas <silas_jfex@nocafe.net>
2021-09-12 20:29:54 +0000

    Let's have reference count for StrBuf objects
    FossilOrigin-Name: 439a734b2d1b09eabd8958d96842d5e9e0db8be215039987fa217f6de349737b

--- src/strbuf.c
+++ src/strbuf.c
@@ -16,7 +16,7 @@ struct StrBuf {
 	char *buf;	/* Buffer that store string (always with null byte) */
 	size_t len;	/* Length of string in buf (don't count null byte )*/
 	size_t max;	/* Length of buf - 1 (cause we don't count null byte */
-	bool attached;  /* Is the buffer attached to the StrBuf object? */
+	int rc;		/* Reference count */
 };
 
 char *strbuf_buf(const StrBuf *s) {
@@ -30,7 +30,7 @@ StrBuf *strbuf_new() {
 	ss->buf[0] = '\0';
 	ss->len = 0;
 	ss->max = INITSIZE;
-	ss->attached = true;
+	ss->rc = 1;
 	return ss;
 }
 
@@ -58,9 +58,15 @@ void strbuf_ncat_c(StrBuf *ss, const char *s, size_t size) {
 	*sb = '\0';
 }
 
-void strbuf_free(StrBuf *ss) {
-	if (ss->attached)
-		free(ss->buf);
+void strbuf_increc(StrBuf *ss) {
+	ss->rc++;
+}
+
+void strbuf_decref(StrBuf *ss) {
+	ss->rc--;
+	if (ss->rc > 0)
+		return;
+	free(ss->buf);
 	free(ss);
 }
 
@@ -71,15 +77,4 @@ size_t strbuf_len(const StrBuf *ss) {
 void strbuf_reset(StrBuf *ss) {
 	ss->len = 0;
 	ss->buf[0] = '\0';
-}
-
-/*
- * Detach a buffer from the StrBuf object.  This is useful when we need to work
- * on the `char *` buffer directly and don't need the StrBuf object anymore.  The
- * caller will have to call strbuf_free() on the StrBuf object and free() on the
- * char * variable when after using the `char *` string.
- */
-char *strbuf_detach(StrBuf *ss) {
-	ss->attached = false;
-	return ss->buf;
 }
