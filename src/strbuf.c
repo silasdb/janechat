d9bb39662e6359c665f78a07a6b255470c75a5f3
silas <silas_jfex@silas.net.br>
2021-09-25 15:00:58 +0000

    Improve signatures: fix a typo in strbuf_incref() definition name and change its signature to return StrBuf* (the object that was just modified). Also, make strbuf_buf() inline (we needed to move struct StrBuf definition to strbuf.h).
    FossilOrigin-Name: 26b4caee5c8eb4a6df532b3d4b7d34d2a29cc75e96f8a88eb6c2f02bbffafb08

--- src/strbuf.c
+++ src/strbuf.c
@@ -7,22 +7,6 @@
 
 #define INITSIZE 256
 
-/*
- * Fat pointer to our String type.  buf length is max+1, because we always make
- * room for the nullbyte, but don't count it in len and max.  StrBuf objects are
- * always guaranteed to have null-terminated strings.
- */
-struct StrBuf {
-	char *buf;	/* Buffer that store string (always with null byte) */
-	size_t len;	/* Length of string in buf (don't count null byte )*/
-	size_t max;	/* Length of buf - 1 (cause we don't count null byte */
-	int rc;		/* Reference count */
-};
-
-char *strbuf_buf(const StrBuf *s) {
-	return s->buf;
-}
-
 StrBuf *strbuf_new() {
 	StrBuf *ss;
 	ss = malloc(sizeof(struct StrBuf));
@@ -70,8 +54,9 @@ void strbuf_ncat_c(StrBuf *ss, const char *s, size_t size) {
 	*sb = '\0';
 }
 
-void strbuf_increc(StrBuf *ss) {
+StrBuf *strbuf_incref(StrBuf *ss) {
 	ss->rc++;
+	return ss;
 }
 
 void strbuf_decref(StrBuf *ss) {
