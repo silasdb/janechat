32bc0f846331ed0c6e96b43898f20abcbaaaa726
silas <silas_jfex@silas.net.br>
2021-11-09 21:56:34 +0000

    If vector is too small, realloc()
    FossilOrigin-Name: dc134d8a7326ed29ff6db2080456880c9e9d869780bc1935e8d94f32b8211727

--- src/vector.c
+++ src/vector.c
@@ -15,8 +15,10 @@ Vector *vector_new() {
 }
 
 void vector_append(Vector *v, void *elem) {
-	assert(v->len < INITSIZE); 
+	if (v->len == v->max) {
+		v->max *= 2;
+		v->elems = realloc(v->elems, (sizeof(void *) * v->max));
+	}
 	v->elems[v->len] = elem;
 	v->len++;
-	/* TODO: realloc() */
 }
