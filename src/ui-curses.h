3e6d170567d4ae6fa7e2ef1a08185423bd4d1205
silas <silas_jfex@nocafe.net>
2021-11-11 22:31:11 +0000

    Implement ui_curses_room_new() to handle new rooms. Adjust other function names. Make only iter() and msg_new() obligatory, so ui-cli is not forced to implement those and future functions.
    FossilOrigin-Name: 43c44d69df0eca8114fd4dd8eeff5ccce39b6e8a6810b7979e56c97a1c4da802

--- src/ui-curses.h
+++ src/ui-curses.h
@@ -3,6 +3,7 @@
 
 void ui_curses_init();
 void ui_curses_iter();
-void ui_curses_new_msg(Room *room, Str *sender, Str *msg);
+void ui_curses_msg_new(Room *room, Str *sender, Str *msg);
+void ui_curses_room_new(Str *roomid);
 
 #endif /* !JANECHAT_UI_CURSES_H */
