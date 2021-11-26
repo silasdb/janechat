762e2acf53263d67944dc642688152bc10e1440f
silas <silas_jfex@nocafe.net>
2021-11-26 23:56:14 +0000

    Reorganize functions so we need less prototypes (basic and private functions come first).
    FossilOrigin-Name: f8f677346c78b2dcf019950bd9ee92f3da4b6ad72a24b6302fb3b6bb99ac903c

--- src/matrix.c
+++ src/matrix.c
@@ -40,13 +40,6 @@ char *token = NULL;
 const char *matrix_server = NULL;
 static void (*event_handler_callback)(MatrixEvent) = NULL;
 
-static void matrix_send(enum HTTPMethod, const char *, const char *,
-	void (*callback)(const char *));
-static json_t *json_path(json_t *root, const char *, ...);
-static const char * json2str_alloc(json_t *);
-static json_t *str2json_alloc(const char *);
-static void process_sync_response(const char *);
-
 CURLM *mhandle = NULL;
 fd_set fdread;
 fd_set fdwrite;
@@ -74,6 +67,128 @@ send_callback(void *contents, size_t size, size_t nmemb, void *userp)
 	return size * nmemb;
 }
 
+static void matrix_send(
+	enum HTTPMethod method,
+	const char *path,
+	const char *json,
+	void (*callback)(const char *))
+{
+	struct callback_info *c = malloc(sizeof(struct callback_info));
+	c->callback = callback;
+	static bool curl_initialized = false;
+	if (!curl_initialized) {
+		 /* TODO: we should enable only what we need */
+		curl_global_init(CURL_GLOBAL_ALL);
+		curl_initialized = true;
+		/*
+		 * TODO: should we call curl_global_cleanup() at the end of the
+		 * program?
+		 */
+	}
+
+	CURL *handle = NULL;
+
+	if (!mhandle)
+		mhandle = curl_multi_init();
+
+	handle = curl_easy_init();
+	Str *aux = str_new();
+
+	c->data = aux;
+	
+	Str *url = str_new();
+	
+	str_append_cstr(url, "https://");
+	assert(matrix_server != NULL);
+	str_append_cstr(url, matrix_server);
+	str_append_cstr(url, ":443");
+	str_append_cstr(url, path);
+
+#if DEBUG_REQUEST
+	printf("DEBUG_REQUEST: url: %s\n", str_buf(url));
+	if (json)
+		printf("DEBUG_REQUEST: json: %s\n", json);
+#endif
+	
+	/*
+	 * TODO: libcurl timeouts with SIGALRM, so we need to caught this signal
+	 * so the program doesn't abort.
+	 */
+	curl_easy_setopt(handle, CURLOPT_TIMEOUT, 60L);
+	curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT, 60L);
+	curl_easy_setopt(handle, CURLOPT_URL, str_buf(url));
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, send_callback);
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)aux);
+	curl_easy_setopt(handle, CURLOPT_PRIVATE, (void *)c);
+
+	switch (method) {
+	case HTTP_POST:
+		/*
+		 * TODO: According to
+		 * https://curl.se/libcurl/c/curl_easy_setopt.html, we cannot
+		 * handle strings longer than 8 MB.  Should we validate the
+		 * string length?
+		 */
+		curl_easy_setopt(handle, CURLOPT_POST, 1L);
+		curl_easy_setopt(handle, CURLOPT_POSTFIELDSIZE, (long)strlen(json));
+		curl_easy_setopt(handle, CURLOPT_COPYPOSTFIELDS, json);
+		break;
+	default:
+		break;
+	}
+
+	curl_multi_add_handle(mhandle, handle);
+	str_decref(url);
+	curl_multi_perform(mhandle, &still_running);
+}
+
+/*
+ * Given a json_t *root object, returns a deep nested object whose path matches
+ * variable argument path. The argument list must end with NULL to indicate it
+ * has finished. To return the first object (jansson preserves the order in
+ * which they are inserted), use "^".
+ *
+ * Example: in order to retrieve object that has jq's path .foo.bar, one should
+ * call this function as json_path(obj, "foo", "bar", NULL)
+ */
+static json_t *json_path(json_t *root, const char *path, ...) {
+	va_list args;
+	va_start(args, path);
+	if (!path)
+		return root;
+	do {
+		if (*path == '^') {
+			void *iter = json_object_iter(root);
+			root = json_object_iter_value(iter);
+		} else {
+			root = json_object_get(root, path);
+		}
+		if (!root)
+			return root;
+		path = va_arg(args, const char *);
+	} while (path != NULL);
+	return root;
+}
+
+static const char *json2str_alloc(json_t *j) {
+	return json_dumps(j, 0);
+}
+
+static json_t *str2json_alloc(const char *s) {
+	json_t *j;
+	/*
+	 * TODO: propagate JSON parsing errors up, so we be able to show it in
+	 * the UI.
+	 */
+	json_error_t error;
+	j = json_loads(s, 0, &error);
+	if (!j) {
+		printf("Error when parsing JSON line %d: %s\n", error.line, error.text);
+		return NULL;
+	}
+	return j;
+}
+
 void matrix_send_message(const Str *roomid, const Str *msg) {
 	Str *url = str_new();
 	str_append_cstr(url, "/_matrix/client/r0/rooms/");
@@ -95,62 +210,6 @@ void matrix_set_server(char *s) {
 
 void matrix_set_token(char *tok) {
 	token = tok;
-}
-
-bool matrix_initial_sync(void) {
-	Str *url = str_new();
-	str_append_cstr(url, "https://");
-	assert(matrix_server != NULL);
-	str_append_cstr(url, matrix_server);
-	str_append_cstr(url, ":443");
-	str_append_cstr(url, "/_matrix/client/r0/sync");
-	str_append_cstr(url, "?filter={\"room\":{\"timeline\":{\"limit\":1}}}");
-	str_append_cstr(url, "&access_token=");
-	str_append_cstr(url, token);
-	CURL *handle = curl_easy_init();
-	assert(handle);
-	CURLcode res;
-	Str *aux = str_new();
-	curl_easy_setopt(handle, CURLOPT_URL, str_buf(url));
-	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, send_callback);
-	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)aux);
-	res = curl_easy_perform(handle);
-	if (res != CURLE_OK) {
-		fprintf(stderr, "curl error: %s\n", curl_easy_strerror(res));
-		return false;
-	}
-	curl_easy_cleanup(handle);
-	str_decref(url);
-	process_sync_response(str_buf(aux));
-	str_decref(aux);
-	return true;
-}
-
-void matrix_sync(void) {
-	if (insync)
-		return;
-	insync = true;
-	Str *url = str_new();
-	str_append_cstr(url, "/_matrix/client/r0/sync");
-	str_append_cstr(url, "?since=");
-	str_append_cstr(url, next_batch);
-	str_append_cstr(url, "&timeout=10000");
-	str_append_cstr(url, "&access_token=");
-	str_append_cstr(url, token);
-	matrix_send(HTTP_GET, str_buf(url), NULL, process_sync_response);
-	str_decref(url);
-}
-
-void matrix_login(const char *server, const char *user, const char *password) {
-	matrix_server = strdup(server);
-	json_t *root = json_object();
-	json_object_set(root, "type", json_string("m.login.password"));
-	json_object_set(root, "user", json_string(user));
-	json_object_set(root, "password", json_string(password));
-	json_object_set(root, "initial_device_display_name", json_string("janechat"));
-	const char *s = json2str_alloc(root);
-	matrix_send(HTTP_POST, "/_matrix/client/r0/login", s, process_sync_response);
-	free((void *)s);
 }
 
 static void process_direct_event(const char *sender, json_t *roomid) {
@@ -198,6 +257,47 @@ static void process_room_event(json_t *item, const char *roomid) {
 		str_decref(event.roomjoin.roomid);
 		str_decref(event.roomjoin.sender);
 	}
+}
+
+enum SelectStatus select_matrix_stdin(void) {
+	struct timeval timeout;
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 100;
+	
+	/* TODO: use curl_multi_timeout()? */
+
+	FD_ZERO(&fdread);
+	FD_ZERO(&fdwrite);
+	FD_ZERO(&fdexcep);
+
+	/* TODO: place it in a better place */
+	FD_SET(0 /* TODO: stdin */, &fdread);
+		
+	curl_multi_fdset(
+		mhandle,
+		&fdread,
+		&fdwrite,
+		&fdexcep,
+		&maxfd);
+	/* TODO: why (maxfd + 2) ?*/
+	int res = select(maxfd + 2, &fdread, &fdwrite, &fdexcep, &timeout);
+	switch (res) {
+	case -1:
+		if (errno == EINTR)
+			return SELECTSTATUS_MATRIXRESUME;
+		perror("select()");
+		abort(); /* TODO */
+		break;
+	case 0:
+		if (still_running)
+			return SELECTSTATUS_MATRIXRESUME;
+		break;
+	default: /* res > 0 */
+		if (FD_ISSET(0, &fdread))
+			return SELECTSTATUS_STDINREADY;
+		break;
+	}
+	return SELECTSTATUS_MATRIXRESUME;
 }
 
 static void process_timeline_event(json_t *item, const char *roomid) {
@@ -371,45 +471,61 @@ static void process_sync_response(const char *output) {
 	
 }
 
-enum SelectStatus select_matrix_stdin(void) {
-	struct timeval timeout;
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 100;
-	
-	/* TODO: use curl_multi_timeout()? */
 
-	FD_ZERO(&fdread);
-	FD_ZERO(&fdwrite);
-	FD_ZERO(&fdexcep);
-
-	/* TODO: place it in a better place */
-	FD_SET(0 /* TODO: stdin */, &fdread);
-		
-	curl_multi_fdset(
-		mhandle,
-		&fdread,
-		&fdwrite,
-		&fdexcep,
-		&maxfd);
-	/* TODO: why (maxfd + 2) ?*/
-	int res = select(maxfd + 2, &fdread, &fdwrite, &fdexcep, &timeout);
-	switch (res) {
-	case -1:
-		if (errno == EINTR)
-			return SELECTSTATUS_MATRIXRESUME;
-		perror("select()");
-		abort(); /* TODO */
-		break;
-	case 0:
-		if (still_running)
-			return SELECTSTATUS_MATRIXRESUME;
-		break;
-	default: /* res > 0 */
-		if (FD_ISSET(0, &fdread))
-			return SELECTSTATUS_STDINREADY;
-		break;
+bool matrix_initial_sync(void) {
+	Str *url = str_new();
+	str_append_cstr(url, "https://");
+	assert(matrix_server != NULL);
+	str_append_cstr(url, matrix_server);
+	str_append_cstr(url, ":443");
+	str_append_cstr(url, "/_matrix/client/r0/sync");
+	str_append_cstr(url, "?filter={\"room\":{\"timeline\":{\"limit\":1}}}");
+	str_append_cstr(url, "&access_token=");
+	str_append_cstr(url, token);
+	CURL *handle = curl_easy_init();
+	assert(handle);
+	CURLcode res;
+	Str *aux = str_new();
+	curl_easy_setopt(handle, CURLOPT_URL, str_buf(url));
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, send_callback);
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)aux);
+	res = curl_easy_perform(handle);
+	if (res != CURLE_OK) {
+		fprintf(stderr, "curl error: %s\n", curl_easy_strerror(res));
+		return false;
 	}
-	return SELECTSTATUS_MATRIXRESUME;
+	curl_easy_cleanup(handle);
+	str_decref(url);
+	process_sync_response(str_buf(aux));
+	str_decref(aux);
+	return true;
+}
+
+void matrix_sync(void) {
+	if (insync)
+		return;
+	insync = true;
+	Str *url = str_new();
+	str_append_cstr(url, "/_matrix/client/r0/sync");
+	str_append_cstr(url, "?since=");
+	str_append_cstr(url, next_batch);
+	str_append_cstr(url, "&timeout=10000");
+	str_append_cstr(url, "&access_token=");
+	str_append_cstr(url, token);
+	matrix_send(HTTP_GET, str_buf(url), NULL, process_sync_response);
+	str_decref(url);
+}
+
+void matrix_login(const char *server, const char *user, const char *password) {
+	matrix_server = strdup(server);
+	json_t *root = json_object();
+	json_object_set(root, "type", json_string("m.login.password"));
+	json_object_set(root, "user", json_string(user));
+	json_object_set(root, "password", json_string(password));
+	json_object_set(root, "initial_device_display_name", json_string("janechat"));
+	const char *s = json2str_alloc(root);
+	matrix_send(HTTP_POST, "/_matrix/client/r0/login", s, process_sync_response);
+	free((void *)s);
 }
 
 void matrix_resume(void) {
@@ -440,126 +556,4 @@ void matrix_resume(void) {
 		str_decref(c->data);
 		free(c);
 	}
-}
-
-static void matrix_send(
-	enum HTTPMethod method,
-	const char *path,
-	const char *json,
-	void (*callback)(const char *))
-{
-	struct callback_info *c = malloc(sizeof(struct callback_info));
-	c->callback = callback;
-	static bool curl_initialized = false;
-	if (!curl_initialized) {
-		 /* TODO: we should enable only what we need */
-		curl_global_init(CURL_GLOBAL_ALL);
-		curl_initialized = true;
-		/*
-		 * TODO: should we call curl_global_cleanup() at the end of the
-		 * program?
-		 */
-	}
-
-	CURL *handle = NULL;
-
-	if (!mhandle)
-		mhandle = curl_multi_init();
-
-	handle = curl_easy_init();
-	Str *aux = str_new();
-
-	c->data = aux;
-	
-	Str *url = str_new();
-	
-	str_append_cstr(url, "https://");
-	assert(matrix_server != NULL);
-	str_append_cstr(url, matrix_server);
-	str_append_cstr(url, ":443");
-	str_append_cstr(url, path);
-
-#if DEBUG_REQUEST
-	printf("DEBUG_REQUEST: url: %s\n", str_buf(url));
-	if (json)
-		printf("DEBUG_REQUEST: json: %s\n", json);
-#endif
-	
-	/*
-	 * TODO: libcurl timeouts with SIGALRM, so we need to caught this signal
-	 * so the program doesn't abort.
-	 */
-	curl_easy_setopt(handle, CURLOPT_TIMEOUT, 60L);
-	curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT, 60L);
-	curl_easy_setopt(handle, CURLOPT_URL, str_buf(url));
-	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, send_callback);
-	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)aux);
-	curl_easy_setopt(handle, CURLOPT_PRIVATE, (void *)c);
-
-	switch (method) {
-	case HTTP_POST:
-		/*
-		 * TODO: According to
-		 * https://curl.se/libcurl/c/curl_easy_setopt.html, we cannot
-		 * handle strings longer than 8 MB.  Should we validate the
-		 * string length?
-		 */
-		curl_easy_setopt(handle, CURLOPT_POST, 1L);
-		curl_easy_setopt(handle, CURLOPT_POSTFIELDSIZE, (long)strlen(json));
-		curl_easy_setopt(handle, CURLOPT_COPYPOSTFIELDS, json);
-		break;
-	default:
-		break;
-	}
-
-	curl_multi_add_handle(mhandle, handle);
-	str_decref(url);
-	curl_multi_perform(mhandle, &still_running);
-}
-
-/*
- * Given a json_t *root object, returns a deep nested object whose path matches
- * variable argument path. The argument list must end with NULL to indicate it
- * has finished. To return the first object (jansson preserves the order in
- * which they are inserted), use "^".
- *
- * Example: in order to retrieve object that has jq's path .foo.bar, one should
- * call this function as json_path(obj, "foo", "bar", NULL)
- */
-static json_t *json_path(json_t *root, const char *path, ...) {
-	va_list args;
-	va_start(args, path);
-	if (!path)
-		return root;
-	do {
-		if (*path == '^') {
-			void *iter = json_object_iter(root);
-			root = json_object_iter_value(iter);
-		} else {
-			root = json_object_get(root, path);
-		}
-		if (!root)
-			return root;
-		path = va_arg(args, const char *);
-	} while (path != NULL);
-	return root;
-}
-
-static const char *json2str_alloc(json_t *j) {
-	return json_dumps(j, 0);
-}
-
-static json_t *str2json_alloc(const char *s) {
-	json_t *j;
-	/*
-	 * TODO: propagate JSON parsing errors up, so we be able to show it in
-	 * the UI.
-	 */
-	json_error_t error;
-	j = json_loads(s, 0, &error);
-	if (!j) {
-		printf("Error when parsing JSON line %d: %s\n", error.line, error.text);
-		return NULL;
-	}
-	return j;
 }
