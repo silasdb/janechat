ca0f51d2ce90329c6ae561069ac974561aa2b1b2
silas <silas_jfex@silas.net.br>
2022-01-21 00:52:08 +0000

    Use a FileInfo struct that encapsulates MIME type and file URI
    FossilOrigin-Name: 09baa38031b117f8310f91041d2fabb5b134553ee15d22a750208ebd5a1a12b1

--- src/matrix.h
+++ src/matrix.h
@@ -60,7 +60,7 @@ void matrix_set_event_handler(void (*callback)(MatrixEvent));
 bool matrix_initial_sync(void);
 void matrix_sync(void);
 void matrix_send_message(const Str *roomid, const Str *msg);
-void matrix_request_file(const Str *uri);
+void matrix_request_file(FileInfo);
 MatrixEvent * matrix_next_event();
 void matrix_set_server(char *token);
 void matrix_set_token(char *token);
