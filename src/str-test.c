b2fe3c7bee62f177acde00a2fdbbc76e9a644bd5
silas <silas_jfex@silas.net.br>
2022-07-13 16:31:12 +0000

    Start integrating cmocka (cmocka.org) to unit test our code.
    FossilOrigin-Name: 56cedc3e117565dcbf06ca25dfbfd3e537419c8bc6e26845a185cf02161a9be7

---
+++ src/str-test.c
@@ -0,0 +1,21 @@
+#include <setjmp.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <cmocka.h>
+
+#include "str.c"
+
+static void str_test_append(void **state) {
+	assert_true(true);
+}
+
+int main(int argc, char *argv[]) {
+	int rc;
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(str_test_append),
+	};
+
+	cmocka_set_message_output(CM_OUTPUT_SUBUNIT);
+
+	rc = cmocka_run_group_tests(tests, NULL, NULL);
+}
