9701b82259025600f674c7657ca64eaa626c7df5
silas <silas_jfex@nocafe.net>
2022-07-14 15:36:17 +0000

    Don't use cmocka: use C's assert() for our tests
    FossilOrigin-Name: e080f7c8e302acd127d3deddcf971c30166904a9e6947064c500f19ac301587e

--- tests/str.test.c
+++ tests/str.test.c
@@ -1,25 +1,16 @@
-#include <setjmp.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <cmocka.h>
+#undef NDEBUG
+#include <assert.h>
 
 #include "../src/str.c"
 
-static void str_append_test(void **state) {
+static void str_append_test() {
 	Str *s = str_new_cstr("abc");
 	str_append_cstr(s, "def");
-	assert_true(strcmp(str_buf(s), "abcdef") == 0);
-	assert_true(str_len(s) == 6);
+	assert(strcmp(str_buf(s), "abcdef") == 0);
+	assert(str_len(s) == 6);
 }
 
 int main(int argc, char *argv[]) {
-	int rc;
-	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(str_append_test),
-	};
-
-	cmocka_set_message_output(CM_OUTPUT_SUBUNIT);
-
-	rc = cmocka_run_group_tests(tests, NULL, NULL);
-	return rc;
+	str_append_test();
+	return 0;
 }
