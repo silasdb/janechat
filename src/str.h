e0707a940bf31d220dc0fe7327a8a0f3690a450b
silas <silas_jfex@silas.net.br>
2022-07-14 21:46:43 +0000

    Delegate UTF-8 string handling to Str objects. But don't implement it yet in str.c, only creates interfaces into str.h.
    FossilOrigin-Name: 91356d6960329c69e73616d7f564a3e435c73c99b479b1c9411c508ee21aadc0

--- src/str.h
+++ src/str.h
@@ -15,6 +15,7 @@ typedef struct Str Str;
 struct Str {
 	char *buf;	/* Buffer that store string (always with null byte) */
 	size_t len;	/* Length of string in buf (don't count null byte )*/
+	int chars;	/* UTF-8 length (don't count null byte) */
 	size_t max;	/* Length of buf - 1 (cause we don't count null byte */
 	int rc;		/* Reference count */
 };
@@ -22,17 +23,21 @@ struct Str {
 inline const char *str_buf(const Str *ss) { return ss->buf; }
 
 #define streq(x, y) (strcmp(x, y) == 0)
+#define str_sc_eq(x, y) (strcmp(str_buf(x), y) == 0)
 Str *str_new(void);
 Str *str_new_cstr(const char *);
 Str *str_new_len(size_t);
 void str_append(Str *ss, const Str *s);
 void str_append_cstr(Str *ss, const char *s);
 void str_append_cstr_len(Str *ss, const char *s, size_t);
+void str_insert_cstr_len(Str *, const char *s, size_t, size_t);
 size_t str_len(const Str *);
 int str_cmp(const Str *, const Str *);
 void str_reset(Str *);
 Str *str_incref(Str *);
 void str_decref(Str *);
-size_t str_len(const Str *);
+size_t str_chars(const Str *);
+char str_char_at(const Str *, size_t);
+void str_remove_char_at(Str *, size_t);
 
 #endif /* !JANECHAT_STR_H */
